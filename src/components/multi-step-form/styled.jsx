907b1080849ec01ba6fee7e29ceda5a4ac0bd908
Yunus <yunusinne@gmail.com>
2023-02-05 03:33:53 +0300

    sidebar component

--- src/components/multi-step-form/styled.jsx
+++ src/components/multi-step-form/styled.jsx
@@ -4,7 +4,7 @@ export const MultiStepForm = styled.div`
 display: flex;
 padding: 12px;
 background-color: var(--white);
-min-height: 50vh;
+min-height: 60vh;
 min-width: 90vw;
 gap:12px;
 box-shadow: 0 5px 10px 10px rgba(0,0,0,0.05);
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,7 +1,20 @@
 
+import * as S from "./styled";
+import {Items} from "./constants"
+
 function Sidebar(){
     return (
-        <div> Sidebar </div>
+        <S.Sidebar>
+            {Items.map((item) => (
+                <S.Item key={item.id}>
+                        <S.ItemNumber>{item.number}</S.ItemNumber>
+                        <S.ItemBody>
+                            <S.Subtitle>{item.subtitle}</S.Subtitle>
+                            <S.Title> {item.title}</S.Title>
+                        </S.ItemBody>
+                </S.Item>
+                    ))}
+        </S.Sidebar>
     )
 }
 
