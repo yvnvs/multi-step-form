d9b14351e268d66dcc8baa69c19bbb59a8c19fb3
Yunus <yunusinne@gmail.com>
2023-02-06 03:31:33 +0300

    step2 component

--- src/components/step-2/index.jsx
+++ src/components/step-2/index.jsx
@@ -1,9 +1,57 @@
+import { useState } from "react"
 import Step from "../step"
+import FormJSON from "../../../form.json"
 
-function Step2(props){
+import ArcadeIcon from "../../assets/icon-arcade.svg"
+import AdvancedIcon from "../../assets/icon-advanced.svg"
+import ProIcon from "../../assets/icon-pro.svg"
+
+import * as S from "./styled"
+
+import {
+    Icons,
+    DEFAULT_PLAN,
+    DEFAULT_BILLING_TYPE,
+    MONTHLY,
+    YEARLY,
+} from "./constants"
+
+
+const { step2 } = FormJSON;
+
+function Step2(props) {
+
+    const [plan,setPlan] = useState(DEFAULT_PLAN);
+
+    const [billingType, setBillingType] = useState(DEFAULT_BILLING_TYPE)
+    
+    const changePlan = (newPlan) => {
+        setPlan(newPlan);
+    }
+
+    const changeBillingType = (newBillingType)=> {
+        setBillingType(newBillingType);
+    }
+
     return (
-        <Step {...props}> 
-        Sidebar 
+        <Step {...props}>
+            <S.Step2>
+                <S.RadioGroup>
+                    {step2[billingType].map((item) => (
+                        <S.RadioLabel key={item.id} isSelected={item.id===plan}>
+                            <S.RadioInput name="plan-type" type="radio" onChange={()=> changePlan(item.id)} />
+                            <S.Icon src={Icons[item.id]} />
+                            <S.Title>{item.title}</S.Title>
+                            <S.Subtitle>{item.price}</S.Subtitle>
+                            {billingType === YEARLY && (<S.Description>{item.description}</S.Description>)}
+                        </S.RadioLabel>
+                    ))}
+                </S.RadioGroup>
+                    <S.BillingGroup>
+                        <S.BillingButton type="button" onClick={()=> changeBillingType(MONTHLY)} isSelected={billingType===MONTHLY }>Monthly</S.BillingButton>
+                        <S.BillingButton type="button" onClick={()=> changeBillingType(YEARLY)} isSelected={billingType===YEARLY }>Yearly</S.BillingButton>
+                    </S.BillingGroup>
+            </S.Step2>
         </Step>
     )
 }
